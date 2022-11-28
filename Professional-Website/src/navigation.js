99ba69a5a4bd9ea50d9ffdd0867cea21d39865e5
Kv139 <ksv14ksv14@gmail.com>
2022-11-27 21:55:37 -0800

    Updated Version

---
+++ Professional-Website/src/navigation.js
@@ -0,0 +1,6 @@
+document.getElementById("toggle").addEventListener("click", function () {
+   // document.getElementById("dropdown").classList.toggle("hidden");  
+    document.getElementById("dropdown").classList.toggle("-right-full");
+    document.getElementById("dropdown").classList.toggle("-right-20");
+    document.getElementById("body").classList.toggle("blur-[1px]");
+})
